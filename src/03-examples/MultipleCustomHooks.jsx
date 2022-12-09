d1a59994298102b3d5ddb820ff63e053b2190ee8
Monica Rivera Pun <mondev811@gmail.com>
2022-12-08 22:53:07 -0800

    MultipleCustomHooks improve display

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { useFetch } from "../hooks/useFetch";
 
-export const MultipleCustomHooks = () => {
-  const { data, isLoading, hasError } = useFetch(
-    "https://bible-api.com/john 3:16"
-  );
+export const MultipleCustomHooks = ({ queryrequest = "john 3:16" }) => {
+  const url = `https://bible-api.com/${queryrequest}`;
+  const { data, isLoading, hasError } = useFetch(url);
   return (
     <>
       <h1>Bible Quotes</h1>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && !hasError && data && <p>{data.text}</p>}
+      {isLoading && (
+        <div className="alert alert-info text-center">Loading...</div>
+      )}
+      {!isLoading && !hasError && !!data && (
+        <>
+          <blockquote className="blockquote text-end">
+            <p className="mb-1">{data.text}</p>
+            <footer className="blockquote-footer">{queryrequest}</footer>
+          </blockquote>
+        </>
+      )}
       {!isLoading && hasError && <p>Error found: {hasError}</p>}
+      <button className="btn btn-primary">Next quote</button>
     </>
   );
 };
