c7d75f9270c67beaadc16a9abc58a8da0682bcac
Monica Rivera Pun <mondev811@gmail.com>
2022-12-10 12:03:43 -0800

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -18,5 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // </React.StrictMode>
   // <SimpleForm />
   // <FormWithCustomHook />
-  <MultipleCustomHooks />
+  // <MultipleCustomHooks />
+  <FocusScreen />
 );
